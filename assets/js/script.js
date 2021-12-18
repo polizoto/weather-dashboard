15c6e061423ed7640c89071134e4f0309ecf01f6
polizoto <polizoto@hotmail.com>
2021-12-17 21:45:16 -0500

    Add JS functions to call Weather API and update city History

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,128 @@
-var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=23.09&lon=113.17&exclude=hourly&appid=db8406e2438a3bda2d57fa0aaa4169c2';
-// ?per_page=5
-// https://api.openweathermap.org/data/2.5/onecall?lat=23.09&lon=113.17&exclude=hourly,daily&appid=db8406e2438a3bda2d57fa0aaa4169c2
-// API KEY: db8406e2438a3bda2d57fa0aaa4169c2
-fetch(requestUrl)
-  .then(function (response) {
+var cityFormEl = document.querySelector("#city-form");
+var searchInputEl = document.querySelector("#search");
+var searchHistoryEl = document.querySelector("#search-history");
+var cityName = ""
+var cityNotFound = false;
+var $LATITUDE = ""
+var $LONGITUDE = ""
+
+// Convert Unix Time to Date
+
+// function timeConverter(UNIX_timestamp){
+//   var date = new Date(UNIX_timestamp * 1000);
+//   var months = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+//   var year = date.getFullYear();
+//   var month = months[date.getMonth()];
+//   var day = date.getDate();
+//   var time = month + '/' + day + '/' + year;
+//   return time;
+// }
+// console.log(timeConverter(1639760400));
+
+// ####### Fetch API Function ####### //  
+
+// var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=35.7721&lon=-78.6386&units=imperial&cnt=1&exclude=minutely,hourly,alerts&appid=db8406e2438a3bda2d57fa0aaa4169c2'
+
+// fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log('Github Repo Issues \n----------');
+//     console.log(data);
+//     // console.log(data.daily[0].dt);
+//   })
+    
+// fetch(requestUrl)
+//   .then(function (response) {
+//     if (response.ok) {
+//    response.json()
+//   .then(function (data) {
+//     console.log('Github Repo Issues \n----------');
+//     console.log(data);
+//     // console.log(data.daily[0].dt);
+//   })
+// } else {
+// //  alert("Error: " + city + " was not found. Try again.")
+//  cityNotFound = true
+// }
+// });
+
+// Check if city is found at the API and update history; if not return a warning message
+            
+var getCity = function (city, event) {
+var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=db8406e2438a3bda2d57fa0aaa4169c2'
+
+fetch(requestUrl).then((response) => {
+  if (response.ok) {
     return response.json();
-  })
-  .then(function (data) {
-    console.log('Github Repo Issues \n----------');
-    console.log(data);
+  } else {
+    throw new Error('This city was not found');
+  }
+})
+.then(function (data) {
+      $LATITUDE = data.coord.lat
+	    $LONGITUDE = data.coord.lon
+      searchInputEl.value = "";
+      var row = $(event.target)
+      .closest(".form-search")
+      .children(".search")
+      .children()
+      updateHistory(cityName, row, $(this)) 
+    })
+    .catch((error) => { 
+    searchInputEl.value = "";
+    console.log(error)
+    return alert("Error: " + city + " was not found. Try again.");
+});
+}
 
-    // // TODO: Loop through the response
-    // for (var i = 0; i < 5; i++) {
-
-    // // TODO: Console log each issue's URL and each user's login
-    //   console.log(data[i]);
-    //   console.log(data[i].html_url);
-    //   console.log(data[i].user.login);
-    // }
+var updateHistory = function (cityName, row, History) {
+  var remove = false
+  var length = row.length
+  $(row).each(function() {
+    var cityHistory = $(this)
+    .text();
+    var cityHistoryItem = $(this)
+    if (cityHistory === cityName) {
+   cityHistoryItem.remove();
+   remove = true;
+    }
   });
+  var cityEl = document.createElement("button");
+  cityEl.classList = "search-results";
+  cityEl.textContent = cityName;
+  searchHistoryEl.prepend(cityEl);  
+  if (row.length === 5 && remove === false) {
+    row[4].remove();
+  }
+}
+
+//
+
+var capitalize = function(str) {
+  const mySentence = str
+  const words = mySentence.split(" ");
+for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+}
+words.join("");
+return cityName = words.toString().replace(/,/g, ' ');;
+}
+
+  // ####### click Function ####### //  
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    cityName = searchInputEl.value.trim();
+    if (cityName) {
+      capitalize(cityName);
+      getCity(cityName, event);
+    } else {
+      return alert("Please enter the name of a city");
+      }
+  };
+
+  // ####### END Function ####### // 
+
+  cityFormEl.addEventListener("submit", formSubmitHandler);
