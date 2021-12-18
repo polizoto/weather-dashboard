28b8de297ad10cbfbf445de6b89d1c9481df6c67
polizoto <polizoto@hotmail.com>
2021-12-18 10:27:52 -0500

    Added support for storing five cities in browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ var cityName = ""
 var cityNotFound = false;
 var $LATITUDE = ""
 var $LONGITUDE = ""
+var weather = {
+  city: [],
+  latitude: [],
+  longitude: []
+};
 
 // Convert Unix Time to Date
 
@@ -32,7 +37,7 @@ var $LONGITUDE = ""
 //     console.log(data);
 //     // console.log(data.daily[0].dt);
 //   })
-    
+
 // fetch(requestUrl)
 //   .then(function (response) {
 //     if (response.ok) {
@@ -49,56 +54,88 @@ var $LONGITUDE = ""
 // });
 
 // Check if city is found at the API and update history; if not return a warning message
-            
-var getCity = function (city, event) {
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=db8406e2438a3bda2d57fa0aaa4169c2'
-
-fetch(requestUrl).then((response) => {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error('This city was not found');
-  }
-})
-.then(function (data) {
-      $LATITUDE = data.coord.lat
-	    $LONGITUDE = data.coord.lon
-      searchInputEl.value = "";
-      var row = $(event.target)
-      .closest(".form-search")
-      .children(".search")
-      .children()
-      updateHistory(cityName, row, $(this)) 
-    })
-    .catch((error) => { 
+              
+var getCity = function (city) {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=db8406e2438a3bda2d57fa0aaa4169c2'
+  
+  fetch(requestUrl).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('This city was not found');
+    }
+  })
+  .then(function (data) {
+    $LATITUDE = data.coord.lat
+    $LONGITUDE = data.coord.lon
+    searchInputEl.value = "";
+    updateHistory(cityName) 
+  })
+  .catch((error) => { 
     searchInputEl.value = "";
     console.log(error)
     return alert("Error: " + city + " was not found. Try again.");
-});
+  });
 }
 
-var updateHistory = function (cityName, row, History) {
+var getWeather = function() {
+  // if no "events" object is in localStorage, create a new object
+  if (localStorage.getItem("weather")) {
+      weather = JSON.parse(localStorage.getItem("weather"));
+      for (let i = 0; i < weather.city.length; i++) {
+        var cityEl = document.createElement("button");
+        cityEl.classList = "search-results";
+        cityEl.setAttribute("data-latitude", weather.latitude[i])
+        cityEl.setAttribute("data-longitude", weather.longitude[i])
+        cityEl.textContent = weather.city[i];
+        searchHistoryEl.append(cityEl);  
+      }
+  }
+}
+  
+var loadWeather = function() {
+  var weather = {
+    city: [],
+    latitude: [],
+    longitude: []
+  };
+  var row = $(searchHistoryEl)
+  .children()
+  $(row).each(function() {
+    var name = $(this).text()
+    var latitude = $(this).attr("data-latitude")
+    var longitude = $(this).attr("data-longitude")
+    weather.city.push(name)
+    weather.latitude.push(latitude)
+    weather.longitude.push(longitude)
+  });
+  localStorage.setItem("weather", JSON.stringify(weather));
+}
+
+var updateHistory = function (cityName) {
   var remove = false
-  var length = row.length
+  var row = $(searchHistoryEl)
+  .children()
   $(row).each(function() {
     var cityHistory = $(this)
     .text();
     var cityHistoryItem = $(this)
     if (cityHistory === cityName) {
-   cityHistoryItem.remove();
-   remove = true;
+    cityHistoryItem.remove();
+    remove = true;
     }
   });
   var cityEl = document.createElement("button");
   cityEl.classList = "search-results";
+  cityEl.setAttribute("data-latitude", $LATITUDE)
+  cityEl.setAttribute("data-longitude", $LONGITUDE)
   cityEl.textContent = cityName;
   searchHistoryEl.prepend(cityEl);  
   if (row.length === 5 && remove === false) {
     row[4].remove();
   }
+  loadWeather();
 }
-
-//
 
 var capitalize = function(str) {
   const mySentence = str
@@ -110,19 +147,17 @@ words.join("");
 return cityName = words.toString().replace(/,/g, ' ');;
 }
 
-  // ####### click Function ####### //  
-
 var formSubmitHandler = function(event) {
     event.preventDefault();
     cityName = searchInputEl.value.trim();
     if (cityName) {
       capitalize(cityName);
-      getCity(cityName, event);
+      getCity(cityName);
     } else {
       return alert("Please enter the name of a city");
       }
   };
 
-  // ####### END Function ####### // 
+  getWeather()
 
   cityFormEl.addEventListener("submit", formSubmitHandler);
